74e2395df5d06e82c99d050f75ea44d7e139f83d
marc-romu <49920661+marc-romu@users.noreply.github.com>
2025-01-05 17:38:10 +0100

    refactor(aistatefulcomponentbase): Remove useless GetPrompt method

--- src/SmartHopper.Components/List/AIListCheckComponent.cs
+++ src/SmartHopper.Components/List/AIListCheckComponent.cs
@@ -59,11 +59,11 @@ namespace SmartHopper.Components.List
 
         protected override System.Drawing.Bitmap Icon => Resources.listcheck;
 
-        protected override string GetPrompt(IGH_DataAccess DA)
-        {
-            // We'll handle prompts directly in ProcessAIResponse
-            return null;
-        }
+        //protected override string GetPrompt(IGH_DataAccess DA)
+        //{
+        //    // We'll handle prompts directly in ProcessAIResponse
+        //    return null;
+        //}
 
         protected override AsyncWorker CreateWorker(Action<string> progressReporter)
         {
