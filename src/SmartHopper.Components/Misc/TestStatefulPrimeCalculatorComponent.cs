baaa47e7d790f83b9e5d979a15a07b78f4e401ab
marc-romu <49920661+marc-romu@users.noreply.github.com>
2025-01-21 15:30:51 +0100

    refactor(componentbase): rename calls to StatefulAsyncComponentBase

--- src/SmartHopper.Components/Misc/TestStatefulPrimeCalculatorComponent.cs
+++ src/SmartHopper.Components/Misc/TestStatefulPrimeCalculatorComponent.cs
@@ -21,7 +21,7 @@ using System.Threading.Tasks;
 
 namespace SmartHopper.Components.Misc
 {
-    public class TestStatefulPrimeCalculatorComponent : AIStatefulAsyncComponentBase
+    public class TestStatefulPrimeCalculatorComponent : StatefulAsyncComponentBase
     {
         public override Guid ComponentGuid => new Guid("C2C612B0-2C57-47CE-B9FE-E10621F18935");
         protected override System.Drawing.Bitmap Icon => null;
@@ -29,7 +29,7 @@ namespace SmartHopper.Components.Misc
 
         public TestStatefulPrimeCalculatorComponent()
             : base("Test Stateful Prime Calculator", "TEST-STATEFUL-PRIME",
-                  "Test component for AIStatefulAsyncComponentBase - Calculates the nth prime number.",
+                  "Test component for StatefulAsyncComponentBase - Calculates the nth prime number.",
                   "SmartHopper", "Examples")
         {
         }
