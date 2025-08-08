9b97c87a3ad74f6637b3f08d16e34a025155e9ff
marc-romu <49920661+marc-romu@users.noreply.github.com>
2025-08-08 14:24:43 +0200

    refactor(aicall): implemented model validation in airequest

--- src/SmartHopper.Infrastructure/AICall/IAIRequest.cs
+++ src/SmartHopper.Infrastructure/AICall/IAIRequest.cs
@@ -33,6 +33,11 @@ namespace SmartHopper.Infrastructure.AICall
         string Model { get; set; }
 
         /// <summary>
+        /// Gets the actual model used.
+        /// </summary>
+        string ModelUsed { get; }
+
+        /// <summary>
         /// Gets or sets the required capabilities to process this request.
         /// </summary>
         AICapability Capability { get; set; }
