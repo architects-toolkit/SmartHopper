38d9a9dbfa60b921e653c0d514cdb47e3c05f2fe
Marc Roca-Musach <49920661+marc-romu@users.noreply.github.com>
2026-07-05 11:43:45 +0200

    Update AI model capabilities for MistralAIProvider

--- src/SmartHopper.Providers.MistralAI/MistralAIProviderModels.cs
+++ src/SmartHopper.Providers.MistralAI/MistralAIProviderModels.cs
@@ -232,7 +232,7 @@ namespace SmartHopper.Providers.MistralAI
                 {
                     Provider = provider,
                     Model = "labs-leanstral-1-5-1",
-                    Capabilities = AICapability.None, // TODO: retrieve capabilities
+                    Capabilities = AICapability.TextInput | AICapability.TextOutput | AICapability.ImageInput | AICapability.FunctionCalling | AICapability.JsonOutput,
                     SupportsStreaming = true,
                     Verified = false,
                     Rank = 9950,
@@ -278,7 +278,7 @@ namespace SmartHopper.Providers.MistralAI
                 {
                     Provider = provider,
                     Model = "mistral-ocr-latest",
-                    Capabilities = AICapability.None, // TODO: retrieve capabilities
+                    Capabilities = AICapability.TextInput | AICapability.TextOutput | AICapability.ImageInput | AICapability.ImageOutput,
                     SupportsStreaming = true,
                     Verified = false,
                     Rank = 9925,
